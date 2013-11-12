24da24cbe01d64344763193567e5df33518c432a
glushkov <djlewap@gmail.com>
2013-11-12 13:53:02 +0400

    postgrs

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+// var express = require("express");
+// var app = express();
+// app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello Worwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwld!');
-});
+// app.get('/', function(request, response) {
+//   response.send('Hello Worwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwld!');
+// });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+// var port = process.env.PORT || 5000;
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
 
 
 // var pg = require('pg').native
@@ -21,3 +21,36 @@ app.listen(port, function() {
 // client.connect();
 // query = client.query('SELECT * FROM mytable');
 // query.on('end', function() { client.end(); });
+
+var express = require('express')
+  , app = express.createServer(express.logger())
+  , pg = require('pg').native
+  , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'
+  , start = new Date()
+  , port = process.env.PORT || 3000
+  , client;
+
+client = new pg.Client(connectionString);
+client.connect();
+
+app.get('/', function(req, res) {
+  var date = new Date();
+
+  client.query('INSERT INTO visits(date) VALUES($1)', [date]);
+
+  query = client.query('SELECT COUNT(date) AS count FROM visits WHERE date = $1', [date]);
+  query.on('row', function(result) {
+    console.log(result);
+
+    if (!result) {
+      return res.send('No data found');
+    } else {
+      res.send('Visits today: ' + result.count);
+    }
+  });
+});
+
+app.listen(port, function() {
+  console.log('Listening on:', port);
+});
+
