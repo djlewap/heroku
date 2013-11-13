ba5000828e2a68003cc992f166ef62d6f9546f0a
glushkov <djlewap@gmail.com>
2013-11-13 12:44:38 +0400

    whoa

--- web.js
+++ web.js
@@ -2,8 +2,10 @@
 // var app = express();
 // app.use(express.logger());
 
+// var html = '<h1 style="position:fixed;top:50%;left:50%;font-size:24px;">САША МОЛОДЕЧИК</h1>'
+
 // app.get('/', function(request, response) {
-//   response.send('Hello Worwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwld!');
+//   response.send(html);
 // });
 
 // var port = process.env.PORT || 5000;
@@ -11,31 +13,13 @@
 //   console.log("Listening on " + port);
 // });
 
+var http = require("http");
 
-// var pg = require('pg').native
-//   , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/fathomless-shore-6467'
-//   , client
-//   , query;
-
-// client = new pg.Client(connectionString);
-// client.connect();
-// query = client.query('SELECT * FROM mytable');
-// query.on('end', function() { client.end(); });
-
-var express = require("express");
-var app = express();
-app.use(express.logger());
-
-var html = '<h1 style="position:fixed;top:50%;left:50%;font-size:24px;">САША МОЛОДЕЧИК</h1>'
-
-app.get('/', function(request, response) {
-  response.send(html);
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
  
 var connectionString = process.env.DATABASE_URL || 'postgres://qwhoxmulnhxiax:vdt5elD96k9K4_g7Nvz73BdQpV@ec2-54-204-2-217.compute-1.amazonaws.com:5432/d24sejgbj31vo2'
 
